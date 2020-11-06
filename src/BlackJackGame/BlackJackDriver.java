3de68740773287e1015f174d956c224220caf388
cliffordhill <cliffordahill11@gmail.com>
2020-11-06 01:05:22 -0600

    Added Gameplay loop
    Added Gameplay loop

--- src/BlackJackGame/BlackJackDriver.java
+++ src/BlackJackGame/BlackJackDriver.java
@@ -6,26 +6,99 @@ public class BlackJackDriver {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-		
+		String playerInput;
+
+		boolean keepPlaying = true; // Game loop terminating case
+
 		Deck deck = new Deck();
 		BlackJackPlayer player1 = new BlackJackPlayer("Player1");
-		BlackJackPlayer dealer= new BlackJackPlayer("Dealer");
-		
-//		player1.AddCardToHand(deck.drawCard());
-//		player1.AddCardToHand(deck.drawCard());
-//		player1.PrintHand();
-//		
-//		dealer.AddCardToHand(deck.drawCard());
-//		dealer.AddCardToHand(deck.drawCard());
-//		dealer.PrintHand();
-		player1.AceTest();
-		player1.PrintHand();
+		BlackJackPlayer dealer = new BlackJackPlayer("Dealer");
 
-		player1.AceTest2();
-		player1.PrintHand();
+		// Both players given 2 cards to start
+		// Game loop
+		while (keepPlaying) {
+			startNewGame(deck, player1, dealer);
+			boolean playersTurn = true;
+			while (playersTurn) {
+
+				System.out.println("Type H for hit, S for stay.");
+				playerInput = input.next().toLowerCase();
+
+				// Draw a card
+				if (playerInput.equals("h")) {
+					System.out.println("Player Hits");
+					hitDeck(deck, player1, true);
+				}
+				// end players turn if they bust or choose to stay
+				if (playerInput.equals("s") || player1.checkBust())
+					playersTurn = false;
+
+			}
+			// Insert Dealers turn
+			while(dealer.getHandValue()< 18) {
+				System.out.println("Dealer Hits");
+				hitDeck(deck,dealer,true);
+			}
+			
+			//Print final results
+			player1.printHand();
+			dealer.printHand();
+			
+			// Check who wins
+			if(checkWinner(player1, dealer)) {
+				System.out.println("You win!");
+			}else {
+				System.out.println("Dealer wins!");				
+			}
+			System.out.println("Do you want to keep playing? Y or N");
+			if (input.next().toLowerCase().equals("n")) {
+				keepPlaying = false;
+			}
+		}
+
+		input.close();
+	}// end of main
+
+	public static void startNewGame(Deck deck, BlackJackPlayer player1, BlackJackPlayer dealer) {
+		player1.discardHand();
+		dealer.discardHand();
+		hitDeck(deck, dealer, false);
+		hitDeck(deck, dealer, true);
+		hitDeck(deck, player1, false);
+		hitDeck(deck, player1, true);
+
+	}
+	/**
+	 * Draws from the deck provided
+	 * @param deck - deck of cards to draw from
+	 * @param player - add the card draw to this player's hand
+	 * @param print - if you want to print players hand or not
+	 */
+	public static void hitDeck(Deck deck, BlackJackPlayer player, boolean print) {
+//		System.out.println(player.getName()+ " hits");
+		player.addCardToHand(deck.drawCard());
+		if (print) {
+			player.printHand();
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * Check if player's hand wins vs the dealer
+	 * @param player1
+	 * @param dealer
+	 * @returns true if player wins otherwise false
+	 */
+	//for now going to work only with dealer vs player
+	public static boolean checkWinner(BlackJackPlayer player1,BlackJackPlayer dealer) {
+		if(player1.checkBust()) {
+			return false;
+		}else if(dealer.checkBust()) {
+			return true;
+		}else if(player1.getHandValue()< dealer.getHandValue()) {
+			return false;
+		}
 		
-		player1.AceTest3();
-		player1.PrintHand();
-		
+		return true;
 	}
 }
--- src/BlackJackGame/BlackJackPlayer.java
+++ src/BlackJackGame/BlackJackPlayer.java
@@ -3,80 +3,79 @@ package BlackJackGame;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class BlackJackPlayer {
 
-	String name;
-	List<Card> cardHand = new ArrayList<Card>();
+	private String name;
+	private List<Card> cardHand = new ArrayList<Card>();
 
 	public BlackJackPlayer(String name) {
 		this.name = name;
 	}
 
-	public void AddCardToHand(Card card) {
+	public void addCardToHand(Card card) {
 		cardHand.add(card);
 	}
 
-	public void PrintHand() {
+	public void printHand() {
 		System.out.print(name + " hand contains");
 		for (Card card : cardHand) {
 			System.out.print(" " + card.getValue() + ":" + card.getSuit());
 		}
-		System.out.println("\n"+ name +" value at :"+ getHandValue());
+		System.out.println("\n" + name + " value at :" + getHandValue());
 
 	}
 
+	//Calculate the players current score based on their hand
 	public int getHandValue() {
 		int value = 0;
-		for(Card card : cardHand) {
-			//convert suits to 10
-			if(card.getValue()>10) {
-				value+= 10;
-			}else if(card.getValue() == 1){
-				value += aceClause(value);
-			}else {
-				value += card.getValue();				
+		int aces = 0;
+		for (Card card : cardHand) {
+			// convert suits to 10
+			if (card.getValue() > 10) {
+				value += 10;
+			} else if (card.getValue() == 1) {
+				aces++;
+			} else {
+				value += card.getValue();
 			}
 		}
+		value += calculateAce(value, aces);
+
 		return value;
 	}
-	
-	private int aceClause(int value) {
-		//runs this 
-		if(value + 11 > 21) {
-			return value + 11;
-		}else {
-			return value + 1;
+
+	//calculate the special rules for aces
+	private int calculateAce(int startValue, int aces) {
+		if (aces ==0) {
+			return 0;
 		}
+		int value = startValue;
+		
+			if (value + 11 > 21) {
+				return aces;
+			} else {
+				return 10 + aces;
+			}
+			
 	}
+	
+	public void discardHand() {
+		cardHand.clear();
+	}
+
+	/**Summary
+	 * Check if player's hand is over 21, if it is, returns true
+	 */
 	public Boolean checkBust() {
-		if (getHandValue()> 21) {
-			return false;
-		} else {
+		if (getHandValue() > 21) {
+			System.out.println("Busted!");
 			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	public void AceTest() {
-		cardHand.clear();		
-		cardHand.add(new Card(1,Suit.CLUB));
-		cardHand.add(new Card(11,Suit.CLUB));
-		cardHand.add(new Card(11,Suit.CLUB));
-		
-	}
-	public void AceTest2() {
-		cardHand.clear();		
-		cardHand.add(new Card(11,Suit.CLUB));
-		cardHand.add(new Card(11,Suit.CLUB));
-		cardHand.add(new Card(1,Suit.CLUB));
-		
-	}
-	public void AceTest3() {
-		cardHand.clear();		
-		cardHand.add(new Card(1,Suit.CLUB));
-		cardHand.add(new Card(11,Suit.CLUB));
-		cardHand.add(new Card(11,Suit.CLUB));
-		cardHand.add(new Card(1,Suit.CLUB));
-		
+
+	public String getName() {
+		return this.name;
 	}
 }
